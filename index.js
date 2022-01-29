4b026d91ca64d919579693fdcee6effd58e11da8
Brique Au Bob <briqueaubob@gmail.com>
2022-01-29 14:50:29 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+import fetch from 'node-fetch';
+import Fastify from 'fastify';
+import cors from 'fastify-cors';
+
+const app = Fastify({
+    logger: true
+});
+app.register(cors, {
+    origin: true
+});
+
+app.get('/', async (req, res) => {
+    try {
+        const url = req.query.url;
+        if(!url) {
+            return res.send({
+                error: 'URL is required'
+            });
+        }
+
+        const body = await (await fetch(url)).text();
+        return res.send(body);
+    } catch (err) {
+        return res.send({
+            error: 'We couldn\'t fetch the url'
+        });
+    }
+});
+
+app.listen(8000);
